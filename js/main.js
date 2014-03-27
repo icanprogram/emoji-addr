e48fbecf5f177dd3928a15dee96e08bbf154cbb8
icanprogram <icanprogramfedoracoins@gmail.com>
2014-03-27 22:33:55 +0100

    First version

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function($){
+	var emoji_mapping = [];
+	var setupEmoji = function(){
+		emoji.include_title = true;
+		
+		$.getJSON("/res/emojimapping.json", function(mapping){
+			emoji_mapping = mapping;
+		});
+	}
+
+	$(function(){
+		setupEmoji();
+		
+		$("#address").on('keyup', function(e){
+			var address = $(this).val(), emojis = "";
+			for(var i=0; i < address.length; i++){
+				var integer = base58.decode(address[i]);
+				emojis += emoji_mapping[integer];
+			}
+			$("#emojis").html(emoji.replace_colons(emojis));
+			$("#emojis-flat").text(emojis);
+		});
+	});
+})(jQuery);
